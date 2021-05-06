7cc489912db14ebb0dded1168a7620e67acc829f
JKimani77 <jkkimani77@gmail.com>
2021-05-06 13:48:33 +0300

    modified the database_uri

--- config.py
+++ config.py
@@ -13,8 +13,7 @@ class Config:
     MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
 
 class ProdConfig(Config):
-    SQLALCHEMY_DATABASE_URI = 'postgresql://injijfrvbmhvig:7c3dba30c6bc1a021ad9423acdc580d2a00594845882a187365920cb5de82871@ec2-52-0-114-209.compute-1.amazonaws.com:5432/d1vg9i5sjavcti
-'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://injijfrvbmhvig:7c3dba30c6bc1a021ad9423acdc580d2a00594845882a187365920cb5de82871@ec2-52-0-114-209.compute-1.amazonaws.com:5432/d1vg9i5sjavcti'
     #SQLALCHEMY_DATABASE_URI = 'postgresql://dolmsujsgcpwth:8a60dac1d0c074445614c8a4c6a3019d50e180e48f07600c204cee12962876c0@ec2-184-72-236-3.compute-1.amazonaws.com:5432/dfkm6g7o8i8v2u'
     #SQLALCHEMY_DATABASE_URI='postgresql://cldhnjtxxhymfa:03575ce94d6db2282ae6a842942a8e34660f538fa5e5b05ecc8a3f48e29fb8ad@ec2-23-23-128-222.compute-1.amazonaws.com:5432/dbeme2dn5uq3kd'
     
